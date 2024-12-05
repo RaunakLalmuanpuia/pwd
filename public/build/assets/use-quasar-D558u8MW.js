455490ba2e50ba2b369aafa02d1c9353f581bc05
Raunak Msegs <Raunak.msegs@gmail.com>
2024-12-05 14:51:42 +0530

    new build

--- public/build/assets/use-quasar-D558u8MW.js
+++
@@ -1,1 +0,0 @@
-import{au as a,av as r}from"./app-DSYfDdqw.js";function s(){return a(r)}export{s as u};
---
+++ public/build/assets/use-quasar-G3CkWjuB.js
@@ -0,0 +1,1 @@
+import{ae as a,af as r}from"./app-BT0dome_.js";function s(){return a(r)}export{s as u};
