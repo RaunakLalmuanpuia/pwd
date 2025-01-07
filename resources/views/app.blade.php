dfd3434b8a72cff89b80066744177c202a89ad80
Raunak <Raunak.msegs@gmail.com>
2025-01-07 20:14:34 +0530

    sdas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script crossorigin=anonymous src=https://securegw-stage.paytm.in/merchantpgpui/checkoutjs/merchants/Resell00448805757124.js></script>
+
+        <link rel="icon" href="https://recruitment.msegs.in/images/pwd_logo.png"
+              type="image/x-icon">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
